a49fd542e05d97651a6a9a290b926e467f78821b
Josef Weldemariam <j.weldemariam@web.de>
2021-05-18 21:19:26 +0200

    transform post object into JSON

--- src/posts/getPosts.js
+++ src/posts/getPosts.js
@@ -4,14 +4,14 @@ const fs = require('fs');
 const dirPath = path.join(__dirname, '../content');
 let postlist = [];
 
-const getPosts = async () => {
-  await fs.readdir(dirPath, (err, files) => {
+const getPosts = () => {
+  fs.readdir(dirPath, (err, files) => {
     if (err) {
       return console.log("Failed to list contents fo directory: " + err);
     }
     files.forEach((file, i) => {
-      let obj = {}
-      let post
+      let obj = {};
+      let post;
       // reads the content of each file, typeof contents = string
       fs.readFile(`${dirPath}/${file}`, "utf8", (err, contents) => {
         // find the index where the meta data starts and ends
@@ -25,7 +25,7 @@ const getPosts = async () => {
         // extract the meta data
         const parseMetadata = ({ lines, metadataIndices }) => {
           if (metadataIndices.length > 0) {
-            let metadata = lines.slice(metadataIndices[0] + 1, metadataIndices[1] - 1);
+            let metadata = lines.slice(metadataIndices[0] + 1, metadataIndices[1] );
             metadata.forEach(line => {
               obj[line.split(": ")[0]] = line.split(": ")[1];
             })
@@ -44,7 +44,7 @@ const getPosts = async () => {
         //read the meta data
         //split the content of the file based on line breaks
         const lines = contents.split("\n");
-        console.log(lines)
+        //console.log(i  + " " + file)
         const metadataIndices = lines.reduce(getMetadataIndices, []); //returns an array with start and end index of metadata
         const metadata = parseMetadata({ lines, metadataIndices }); // returns an object with the metadata
         const content = parseContent({ lines, metadataIndices }); // 
@@ -56,6 +56,10 @@ const getPosts = async () => {
           content: content ? content : "no content"
         };
         postlist.push(post);
+        if( i === files.length - 1){
+          let data = JSON.stringify(postlist);
+          fs.writeFileSync("src/posts/posts.json", data)
+        }
       })
     })
   })
