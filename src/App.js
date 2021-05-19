e598b9dd953145ca1eec520c92e99c147de65142
Josef Weldemariam <j.weldemariam@web.de>
2021-05-20 00:31:30 +0200

    remove useless imports and routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 
 import './App.css'
 import Header from './components/header/header';
 //import Body from './components/body/body';
 import Footer from './components/footer/footer';
-import Homepage from './components/pages/homepage';
-import MediaQuerie from './components/pages/mediaQuerie';
-import GPG from './components/pages/gpg';
-import CSS from './components/pages/css';
-import Intro from './components/pages/MERN/intro';
+import Homepage from './pages/homepage';
+
 
 
 
@@ -22,29 +19,11 @@ export default function App() {
 
         <Route exact path="/" component={Homepage} />
 
-        <p>
-          <Link to="/mediaQuerie" style={{ textDecoration: "none", color: "white", fontSize: "large" }}>Media querie {new Date().toLocaleDateString()}</Link>
-        </p>
+        
 
-        <Route exact path="/mediaQuerie" component={MediaQuerie} />
+       
 
-        <p>
-          <Link to="/gpg" style={{ textDecoration: "none", color: "white" }}>GPG</Link>
-        </p>
-
-        <Route exact path="/gpg" component={GPG} />
-
-        <p>
-          <Link to="/css" style={{ textDecoration: "none", color: "white" }}>CSS</Link>
-        </p>
-
-        <Route exact path="/css" component={CSS} />
-
-        <p>
-          <Link to="/mern-intro" style={{ textDecoration: "none", color: "white" }}>Mern Intro</Link>
-        </p>
-
-        <Route exact path="/mern-intro" component={Intro} />
+        
 
         
         <Footer />
