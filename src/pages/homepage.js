0e47a593c3fe706a905dabe50b6e64ee9ca6b6c0
Josef Weldemariam <j.weldemariam@web.de>
2021-05-20 22:15:14 +0200

    remove useless header

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -9,9 +9,7 @@ export default function homepage() {
       Hi! I'm Josef. In the past I did my bachelor in math. Since then I have been working with various programming languages and concepts. I will post about these at regular intervals. From time to time, non-programming topics will come up as well. Just everything I'm interested in :)
       
       <Layout>
-        <h1 className="postlist">List of posts go here</h1>
-        <PostList />
-          
+        <PostList />         
       </Layout>
     </div>
   )
