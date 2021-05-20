5ccad0de6c1707ca93f79c91ad08a8733488ec00
Josef Weldemariam <j.weldemariam@web.de>
2021-05-20 22:13:04 +0200

    add new postlist component

--- src/components/postlist.js
+++ src/components/postlist.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import Markdown from 'react-markdown';
 
-import Posts from '../posts/posts.json';
+import postlist from '../posts/posts.json';
+import './style.css';
 
-const postlist = () => {
-  console.log(Posts);
+const Postlist = () => {
+  const excerptList = postlist.map(post => {
+    return post.content.split(" ").slice(0,15).join(" ")
+  })
+  console.log(excerptList)
   return (
     <div className="postlist">
-      
+      <h1 className="title">All Posts</h1>
+      {postlist.length &&
+        postlist.map((post, i) => {
+          return (
+            <div className="post" key={post.id}>
+              <h3>{post.title}</h3>
+              <small>Published on {post.date} by {post.author}</small>
+              <hr />
+              <div className="content">
+              <Markdown children={excerptList[i] + " ..."} />
+              read more
+              </div>
+            </div>
+          )
+        })}
     </div>
   )
 }
 
-export default postlist;
+export default Postlist;
