82dd3f4d7bf85515149c84d3f0282e9b9ab85809
Josef Weldemariam <j.weldemariam@web.de>
2021-05-22 12:10:31 +0200

    wrap about text into div for styling

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -6,11 +6,14 @@ import PostList from '../components/postlist';
 export default function homepage() {
   return (
     <div>
-      Hi! I'm Josef. In the past I did my bachelor in math. Since then I have been working with various programming languages and concepts. I will post about these at regular intervals. From time to time, non-programming topics will come up as well. Just everything I'm interested in :)
-      
-      <Layout>
-        <PostList />         
-      </Layout>
+      <div className="about">
+        Hi! I'm Josef. In the past I did my bachelor in math. Since then I have been working with various programming languages and concepts. I will post about these at regular intervals. From time to time, non-programming topics will come up as well. Just everything I'm interested in :)
+      </div>
+      <div>
+        <Layout>
+          <PostList />
+        </Layout>
+      </div>
     </div>
   )
 }
