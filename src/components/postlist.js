8365bfa544ea2d23e87f7304243f065a11933b77
Josef Weldemariam <j.weldemariam@web.de>
2021-05-22 12:09:16 +0200

    add Router

--- src/components/postlist.js
+++ src/components/postlist.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import Markdown from 'react-markdown';
 
 import postlist from '../posts/posts.json';
@@ -10,23 +11,25 @@ const Postlist = () => {
   })
   console.log(excerptList)
   return (
-    <div className="postlist">
-      <h1 className="title">All Posts</h1>
-      {postlist.length &&
-        postlist.map((post, i) => {
-          return (
-            <div className="post" key={post.id}>
-              <h3>{post.title}</h3>
-              <small>Published on {post.date} by {post.author}</small>
-              <hr />
-              <div className="content">
-              <Markdown children={excerptList[i] + " ..."} />
-              read more
+    <Router>
+      <div className="postlist">
+        <h1 className="title">All Posts</h1>
+        {postlist.length &&
+          postlist.map((post, i) => {
+            return (
+              <div className="post" key={post.id}>
+                <h3>{post.title}</h3>
+                <small>Published on {post.date} by {post.author}</small>
+                <hr />
+                <div className="content">
+                  <Markdown children={excerptList[i] + " ..."} />
+                  read more
+                </div>
               </div>
-            </div>
-          )
-        })}
-    </div>
+            )
+          })}
+      </div>
+    </Router>
   )
 }
 
